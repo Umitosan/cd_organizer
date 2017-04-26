7cd001b90170bcee991746f7f538e3a77eda4f5d
Dominic Brown and Monique St Laurent <monique@>
2017-04-26 16:20:25 -0700

    add minimal front end to add artists and cds from user input

--- app.rb
+++ app.rb
@@ -1,10 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require './lib/cd_organizer'
+require './lib/cd'
+require './lib/artist'
 require 'pry'
 
 also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+post('/cd-success') do
+  my_cd = Cd.new(params.fetch('cd-name'))
+  my_cd.save()
+  @thiscd = my_cd.title()
+  erb(:cd_success)
+end
+
+post('/artist-success') do
+  my_artist = Artist.new(params.fetch('artist-name'))
+  my_artist.save()
+  @thisartist = my_artist.name()
+  erb(:artist_success)
 end
